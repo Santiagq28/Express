ac7a151952cdc6dba2aebe2a6e024dc061b2cd17
Santiagq28 <saguevaraq@ufpso.edu.co>
2026-03-03 07:21:20 -0600

    More functions

--- src/router/apuesta.route.js
+++ src/router/apuesta.route.js
@@ -17,7 +17,7 @@ router.get("/save", async (req,res)=> {
 
     let a = await redis.set('info:192203',
         JSON.stringify(json), {
-            EX:60
+            EX:300
         })
     res.send(a);
 })
@@ -27,6 +27,50 @@ router.get("/get", async (req,res)=>{
     const json = JSON.parse(data)
     console.log(json)
     res.send(json)
+})
+
+router.get("/update", async (req,res)=>{
+    const edad = 19;
+    const data = await redis.get("info:192203");
+    if(!data){
+        return res.json({'success': false, 'data':[], 'msg': 'Not found'},404)
+    }
+    let json = JSON.parse(data);
+    json.edad = edad;
+    const response = await redis.set('info:192203', JSON.stringify(json), {
+        EX: 300
+    });
+    const r = await redis.get("info:192203");
+    res.json({"success": response === 'OK', data :r, msg: response }, 200);
+})
+
+
+router.get('/hset', async (req,res)=> {
+    const response = await redis.hSet('info:192792', {
+        'name': 'juan',
+        'lastname': 'quintero',
+        'age': 16
+    });
+
+    await redis.expire('info:192792', 300)
+    res.send(response)
+})
+
+router.get("/delete", async (req, res) => {
+    const data = await redis.del('info:192792')
+    res.send(data)
+})
+
+router.get('/getHash', async (req, res)=>{
+    
+    //const data = await redis.hDel('info:192792', 'age')
+    //const response = await redis.hGetAll('info:192792');
+    //res.send(response)
+    
+    const response = await redis.hGetAll('info:192792');
+    const ttl = await redis.ttl('info:192792')
+    res.json({success: true, data : response, ttl})
+
 })
 
 export default router;
